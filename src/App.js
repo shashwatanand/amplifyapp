d5fb37036b351710478cc3594a315a63b772eda5
shashwatanand <shashwat.anand@gmail.com>
2019-05-05 16:05:53 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sample React app from Shashwat Anand dployed on AWS aws amplify.
-        </p>
+        <p>Sample React app from Shashwat Anand dployed on AWS aws amplify.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
